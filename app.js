3f8b4c10535a2b0ac0c8973479c66980f88247d8
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-10-18 21:44:19 +0600

    json file

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const app = express();
 const userRouter = require("./routes/users.route");
 
 app.use("/api/user", userRouter);
+app.use("/register", (req, res)=>{
+  // res.status(200).json({
+  // //   // "name": "Md. Abdul Alim",
+  // //   // "message": " I am register Page",
+  // //   // statusCode: 200,
+  // // })
+  res.redirect("/login")
+});
+app.use("/login", (req, res)=>{
+  res.send("I am  login page");
+});
 
 app.use("/", (req, res) => {
-  res.send("I am a get request at home");
+  res.send("I am  home page");
 });
 
 app.use((req, res) => {
